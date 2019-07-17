9dbc526880aa9b43c88f1053c061c6ca42a3c4fa
horwardL <hwloh14@gmail.com>
2019-07-17 11:44:43 -0400

    new file:   mysite/polls/templates/polls/detail.html 	new file:   mysite/polls/templates/polls/index.html 	new file:   mysite/polls/templates/polls/results.html 	modified:   mysite/polls/tests.py 	modified:   mysite/polls/urls.py 	modified:   mysite/polls/views.py

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -2,8 +2,17 @@ from django.urls import path
 
 from . import views
 
+app_name = 'polls'
 urlpatterns = [
     # if user go to url '' follow by it,
     # then goto views.py and call index function
-    path('', views.index, name='index'),
+
+    # ex: /polls/
+    path('', views.IndexView.as_view(), name='index'),
+    # ex: /polls/5
+    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
+    # ex: /polls/5/results/
+    path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
+    # ex: /polls/5/votes/
+    path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
